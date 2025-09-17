cf9e788232652949857e463c85d3b917fc1614b5
Võ Văn Tú <163757956+anhtudayne@users.noreply.github.com>
2025-09-17 08:15:01 +0700

    Delete MessageReadStatusId class from MessageReadStatus

--- backend/dqtbook_backend/src/main/java/qdt/hcmute/vn/dqtbook_backend/model/MessageReadStatus.java
+++ backend/dqtbook_backend/src/main/java/qdt/hcmute/vn/dqtbook_backend/model/MessageReadStatus.java
@@ -3,9 +3,7 @@ package qdt.hcmute.vn.dqtbook_backend.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -29,27 +27,3 @@ public class MessageReadStatus {
     @Column(name = "read_at")
     private Instant readAt;
 }
-
-@Embeddable
-class MessageReadStatusId implements Serializable {
-    @Column(name = "message_id", nullable = false)
-    private Long messageId;
-
-    @Column(name = "user_id", nullable = false)
-    private Integer userId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MessageReadStatusId that = (MessageReadStatusId) o;
-        return Objects.equals(messageId, that.messageId) && Objects.equals(userId, that.userId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(messageId, userId);
-    }
-}
-
-
